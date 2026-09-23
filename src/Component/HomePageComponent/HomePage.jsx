38a78bc630dff2890f55fda11f944ed65e4eaf3a
Biniyam Tamiru <biniamtamiru020@gmail.com>
2026-09-23 05:18:02 -0700

    Update homepage and menu items

---
+++ src/Component/HomePageComponent/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./HomePage.css";
+
+import Directory from "../directory/directory";
+
+
+const HomePage =()=>(
+    <div className='homePage'>
+        <Directory/>
+    </div>
+
+);
+
+export default HomePage;
